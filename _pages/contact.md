fd91e996abbc0d7fb12af3c7d6c39ba59896bbe1
Your Name <you@example.com>
2020-07-12 17:41:22 -0700

    new website goin' up!

---
+++ _pages/contact.md
@@ -0,0 +1,7 @@
+---
+layout: single
+title: Contact
+---
+
+- Send me an [email](mailto:ativarovsky@gmail.com)
+- Find me on [LinkedIn](https://www.linkedin.com/in/alice-tivarovsky-56281131/)
---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,25 @@
+var store = [{
+        "title": "Hello (blog)World",
+        "excerpt":"Why I created this blog… …basically because everyone has a blog. Your mom, your best friend, your cat probably has a blog. As always, I’m late to the party. How I created this blog… …short answer: with great difficulty. …slightly longer answer: I knew very little about html, css, java,...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/blog/2020/07/hello-blogworld/",
+        "teaser": null
+      },{
+        "title": "Summarizing Covid-19 with flexdashboard",
+        "excerpt":"          screenshot from jun 16 2020   For those following the Covid-19 trajectories, this flexdashboard not a novel concept (pun not intended). My version is adapted from Rami Krispin’s original and uses his coronavirus package. Check it out here.   ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/blog/2020/07/covid_dashboard/",
+        "teaser": null
+      },{
+        "title": "Phthalate Exposure in U.S. Women of Reproductive Age - an NHANES Review, Part I",
+        "excerpt":"Motivation Perhaps I’m the type of person that worries excessively about things outside of their control, but I’ve long been concerned about plastics leaching into our bodies from things like food packaging and personal care products. Some of these anxieties might be justified. A recent analysis of bio-specimen data from...","categories": [],
+        "tags": ["Environment","NHANES"],
+        "url": "http://localhost:4000/phthalates/",
+        "teaser": null
+      },{
+        "title": "Phthalate Exposure in U.S. Women of Reproductive Age - an NHANES Review, Part II",
+        "excerpt":"Motivation In Part I of this post, we looked at what phthalates are, how humans interact with them, how much we have in our bodies, and how those levels have changed over time. Here, we’ll attempt to answer Question 2: Are phthalate levels disproportionately distributed through the population? Namely, is...","categories": [],
+        "tags": ["Environment","NHANES"],
+        "url": "http://localhost:4000/phthalates-II/",
+        "teaser": null
+      }]
