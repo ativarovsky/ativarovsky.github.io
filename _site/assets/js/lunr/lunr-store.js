32b552bf95415d1d6e481dcde230f31bddb91295
Your Name <you@example.com>
2020-08-19 16:48:54 -0700

    attempt to fix spacing issues

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -5,12 +5,6 @@ var store = [{
         "url": "http://localhost:4000/blog/2020/07/hello-blogworld/",
         "teaser": null
       },{
-        "title": "Summarizing Covid-19 with flexdashboard",
-        "excerpt":"          screenshot from jun 16 2020   For those following the Covid-19 trajectories, this flexdashboard not a novel concept (pun not intended). My version is adapted from Rami Krispin’s original and uses his coronavirus package. Check it out here.   ","categories": [],
-        "tags": [],
-        "url": "http://localhost:4000/blog/2020/07/covid_dashboard/",
-        "teaser": null
-      },{
         "title": "Phthalate Exposure in U.S. Women of Reproductive Age - an NHANES Review, Part I",
         "excerpt":"Motivation Perhaps I’m the type of person that worries excessively about things outside of their control, but I’ve long been concerned about plastics leaching into our bodies from things like food packaging and personal care products. Some of these anxieties might be justified. A recent analysis of bio-specimen data from...","categories": [],
         "tags": ["Environment","NHANES"],
@@ -27,5 +21,11 @@ var store = [{
         "excerpt":"“This is my favorite part about analytics: Taking boring flat data and bringing it to life through visualization.” – John Tukey Motivation The last two posts were a bit dense so we’ll try to keep it simple here. Let’s start with a morbid stat: if you’re younger than 30 and...","categories": ["data visualization","ggplot2","injuries"],
         "tags": [],
         "url": "http://localhost:4000/data%20visualization/ggplot2/injuries/car-crashes/",
+        "teaser": null
+      },{
+        "title": "Summarizing Covid-19 with flexdashboard",
+        "excerpt":"          screenshot from jun 16 2020   For those following the Covid-19 trajectories, this flexdashboard not a novel concept (pun not intended). My version is adapted from Rami Krispin’s original and uses his coronavirus package. Check it out here.   ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/blog/2020/07/covid_dashboard/",
         "teaser": null
       }]
